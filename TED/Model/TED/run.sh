9086466b80106f053aa286a2be35a6f1c898dcb3
doubleblindpolicy <92842452+doubleblindpolicy@users.noreply.github.com>
2022-01-20 23:30:05 +0800

    Add files via upload

--- TED/Model/TED/run.sh
+++ TED/Model/TED/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-dataset="T20H"
+dataset="DBLP"
 folder="data/${dataset}/"
+pattern_file="${folder}pattern/"
 node_file="${folder}node.dat"
 config_file="${folder}config.dat"
 link_file="${folder}link.dat"
 label_file="${folder}label.dat"
 emb_file="${folder}emb.dat"
 
-rpt="PCC,PCCC,PCIC,PCPC,PCPCC"
-meta="0,2,3,4" # Choose the meta-paths used for training. Suppose the targeting node type is 1 and link type 1 is between node type 0 and 1, then meta="1" means that we use meta-paths "101".
+
 size=50
 nhead="8"
 dropout=0.4
@@ -17,41 +17,38 @@ neigh_por=0.6
 lr=0.005
 weight_decay=0.0005
 batch_size=256
-epochs=300
+epochs=500
 device="cuda"
 
 attributed="True"
 supervised="True"
 
 
-for ratio in '5v5' '4v6' '3v7' '2v8' '1v9'
-do
-if [ "${dataset}" = "T15S" ] || [ "${dataset}" = "T20H" ]
+if [ "${dataset}" = "T20H" ] || [ "${dataset}" = "T15S" ]
 then
+    ratio="5v5"
     label_file="${folder}label_TaxPayer${ratio}.dat"
-else
-    label_file="label.dat"
+    python3 src/T20HModel/main.py --node=${node_file} --link=${link_file} --config=${config_file} --label=${label_file} --output=${emb_file} --device=${device} --meta=${meta} --size=${size} --nhead=${nhead} --dropout=${dropout} --neigh-por=${neigh_por} --lr=${lr} --weight-decay=${weight_decay} --batch-size=${batch_size} --epochs=${epochs} --ratio=${ratio} --attributed=${attributed} --supervised=${supervised}
+elif [ "${dataset}" = "PubMed" ]
+then
+    rpt="CDD,GDD,SDD,GDCD,SDCD,SDGD"
+    nhead="6"
+    meta="1,2,4,8"
+    python src/PubMedModel/main.py --node=${node_file} --link=${link_file} --config=${config_file} --label=${label_file} --output=${emb_file} --device=${device} --rpt=${rpt} --meta=${meta} --size=${size} --nhead=${nhead} --dropout=${dropout} --neigh-por=${neigh_por} --lr=${lr} --weight-decay=${weight_decay} --batch-size=${batch_size} --epochs=${epochs} --attributed=${attributed} --supervised=${supervised}
+elif [ "${dataset}" = "DBLP" ]
+then
+    rpt="VAA,PAA,YAA,PPAA"
+    nhead="4"
+    meta="1,2,3,4,5"
+    python src/DBLPModel/main.py --node=${node_file} --link=${link_file} --config=${config_file} --label=${label_file} --output=${emb_file} --device=${device} --rpt=${rpt} --meta=${meta} --size=${size} --nhead=${nhead} --dropout=${dropout} --neigh-por=${neigh_por} --lr=${lr} --weight-decay=${weight_decay} --batch-size=${batch_size} --epochs=${epochs} --attributed=${attributed} --supervised=${supervised}    
 fi
 
-python3 src/main.py --node=${node_file} --link=${link_file} --config=${config_file} --label=${label_file} --output=${emb_file} --device=${device} --meta=${meta} --size=${size} --nhead=${nhead} --dropout=${dropout} --neigh-por=${neigh_por} --lr=${lr} --weight-decay=${weight_decay} --batch-size=${batch_size} --epochs=${epochs} --ratio=${ratio} --attributed=${attributed} --supervised=${supervised}
 
-name="${folder}/"
-if [ "${attributed}" = "True" ]
+if [ "${dataset}" = "T20H" ] || [ "${dataset}" = "T15S" ]
 then
-    name="${name}Att"
+    name="${folder}Att_Sup_${ratio}.emb.dat"
 else
-    name="${name}UnAtt"
+    name="${folder}Att_Sup.emb.dat"
 fi
-if [ "${supervised}" = "True" ]
-then
-    if [ "${dataset}" = "T20H" ] || [ "${dataset}" = "T15S" ]
-    then
-        name="${name}_Sup_${ratio}.emb.dat"
-    else
-        name="${name}_UnSup_${ratio}.emb.dat"
-    fi
-else
-    name="${name}_UnSup.emb.dat"
-fi
+
 cp "${emb_file}" "${name}"
-done
